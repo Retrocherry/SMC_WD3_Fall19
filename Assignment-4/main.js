03612d36dbf40145b8ac7f8985c12c9853f7cdb2
Gabe Silverstein <gabesilverstein@gmail.com>
2019-09-29 16:08:51 -0700

    Assignment 4: Commit 12
    + Made forms method POST
    + Added browser confirmation message on FORM SUBMIT
    note: unessesary js

---
+++ Assignment-4/main.js
@@ -0,0 +1,27 @@
+var form  = document.getElementsByTagName('form')[0];
+var email = document.getElementById('mail');
+var error = document.querySelector('.error');
+
+email.addEventListener("input", function (event) {
+  // Each time the user types something, we check if the
+  // email field is valid.
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    error.innerHTML = ""; // Reset the content of the message
+    error.className = "error"; // Reset the visual state of the message
+  }
+}, false);
+form.addEventListener("submit", function (event) {
+  // Each time the user tries to send the data, we check
+  // if the email field is valid.
+  if (!email.validity.valid) {
+    
+    // If the field is not valid, we display a custom
+    // error message.
+    error.innerHTML = "Email req";
+    error.className = "error active";
+    // And we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+}, false);
