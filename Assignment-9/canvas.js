06fbba854316ac7be90c35da4e7c0793745e40a5
Gabe Silverstein <gabesilverstein@gmail.com>
2019-11-17 15:59:23 -0800

    A9: C3 - Commented old code
    * FORMATTING
    
    HTML
     - counter id with starting 0
    
    CANVAS JS
    + Added counterElem for ID and listener
    + counterELem amountofClicks to display in HTML when click in ball

--- Assignment-9/canvas.js
+++ Assignment-9/canvas.js
@@ -1,8 +1,74 @@
+// var canvas = document.getElementById("Game");
+// var ctx = canvas.getContext("2d");
+
+// var ballX = canvas.width / 2;
+// var ballY = canvas.height / 2;
+// var ballColor = 'rgb(0, 155, 155)';
+// var ballRadius = 50;
+
+// var speedX = 5;
+// var speedY = 3;
+
+// var directionDown = true;
+// var directionRight = true;
+
+// function animate() {
+//     ctx.fillStyle = 'rgba(225, 235, 222, 0.3)'; //color of background
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.beginPath();
+//     ctx.fillStyle = ballColor;
+//     ctx.strokeStyle = 'black';
+//     ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+//     ctx.fill();
+//     ctx.stroke();
+//     ctx.closePath();
+
+//     if (ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0) {
+//         directionRight = !directionRight;
+//     }
+
+//     if (directionRight) {
+//         ballX = ballX + speedX;
+//     } else {
+//         ballX = ballX - speedX;
+//     }
+
+
+//     if (ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
+//         directionDown = !directionDown;
+//     }
+//     if (directionDown) {
+//         ballY = ballY + speedY;
+//     } else {
+//         ballY = ballY - speedY;
+//     }
+
+//     window.requestAnimationFrame(animate);
+// }
+
+// animate();
+
+
+
+// canvas.addEventListener("click", function (event) {
+
+//     var distX = Math.abs(ballX - event.clientX);
+//     var distY = Math.abs(ballY - event.clientY);
+
+
+//     if (distX < ballRadius && distY < ballRadius) {
+//         console.log('FIRE!!!!!!');
+//         // amountOfClicks = amountOfClicks + 1;
+//     }
+
+// })
+
 var canvas = document.getElementById("Game");
 var ctx = canvas.getContext("2d");
 
-var ballX = canvas.width/2;
-var ballY = canvas.height/2;
+var ballX = canvas.width / 2;
+var ballY = canvas.height / 2;
 var ballColor = 'rgb(0, 155, 155)';
 var ballRadius = 50;
 
@@ -12,37 +78,35 @@ var speedY = 3;
 var directionDown = true;
 var directionRight = true;
 
-function animate(){
+function animate() {
     ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
     ctx.fillStyle = ballColor;
     ctx.strokeStyle = 'black';
-    ctx.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
+    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
-    if(ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0 ) {
+    if (ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0) {
         directionRight = !directionRight;
     }
 
-    if(directionRight){
+    if (directionRight) {
         ballX = ballX + speedX;
-    }
-    else {
+    } else {
         ballX = ballX - speedX;
     }
 
 
-    if(ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
+    if (ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
         directionDown = !directionDown;
     }
-    if(directionDown){
+    if (directionDown) {
         ballY = ballY + speedY;
-    }
-    else {
+    } else {
         ballY = ballY - speedY;
     }
 
@@ -51,14 +115,19 @@ function animate(){
 
 animate();
 
-canvas.addEventListener("click", function(event){
+var counterElem = document.getElementById('counter');
+
+canvas.addEventListener("click", function (event) {
 
     var distX = Math.abs(ballX - event.clientX);
     var distY = Math.abs(ballY - event.clientY);
+    // var distX = Math.abs(ballX - event.canvasX);
+    // var distY = Math.abs(ballY - event.canvasY);
 
-    if(distX < ballRadius && distY < ballRadius){
-        console.log('FIRE!!!!!!');
+    if (distX < ballRadius && distY < ballRadius) {
+        console.log('+1 Clicks');
+        amountOfClicks = amountOfClicks + 1;
+        console.log(amountOfClicks);
+        counterElem.innerHTML = amountOfClicks;
     }
-    
 })
-
--- Week_12/canvas.js
+++ Week_12/canvas.js
@@ -1,68 +1,131 @@
+// var canvas = document.getElementById("Game");
+// var ctx = canvas.getContext("2d");
+
+// var ballX = canvas.width / 2; //this will give us the x position of any ball. our position can be described by 2 or 3 dimensions. 
+// var ballY = canvas.height / 2; //center for Y
+// var ballColor = 'rgb(0, 155, 155)'
+// var ballRadius = 50;
+
+// //PHYSICS
+// var speedX;
+// var speedY; //going to tell me how many pixels it will move on the Y axis on the frame
+// var ballRadius;
+
+// function animate() {
+
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     ctx.beginPath();
+//     ctx.fillStyle = ballColor;
+//     ctx.strokeStyle = 'black';
+//     ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+//     ctx.fill();
+//     ctx.stroke();
+//     ctx.closePath();
+
+//     ballX = ballX + speedX;
+//     ballY = ballY + speedY;
+
+//     if (ballX + ballRadius >= canvas.width) {
+//         directionRight = true;
+
+//     }
+//     if (ballX <= 0) {
+//         directionRight = true;
+//     }
+
+//     if (directionRight) {
+//         ballX = ballX + speedX;
+//     } else {
+//         ballX = ballX - speedX;
+//     }
+
+
+
+//     if (ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) { // || = OR
+//         directionDown = false;
+//     }
+//     if (ballY - ballRadius <= 0) {
+//         directionDown = true;
+
+//     }
+
+
+//     window.requestAnimationFrame(animate) //our browser is going to request the next animation frame. This will be defined by (animate) function
+// }
+
+// animate();
+
+// canvas.addEventListener("click", function (e) { //the peramter that is being passed to it is that event itself. 
+//             console.log(event);
+
+//             var distX = Math.abs(ballX - event.clientX);
+//             var distY = Math.abs(ballY - event.clientY);
+
+//             if (distX < ballRadius && distY < ballRadius) {
+//                 console.log('FIRE@@@@@@@!!!!!!!!!!!!!')
+//             }
+//         }
+
 var canvas = document.getElementById("Game");
 var ctx = canvas.getContext("2d");
 
-var ballX = canvas.width / 2; //this will give us the x position of any ball. our position can be described by 2 or 3 dimensions. 
-var ballY = canvas.height / 2; //center for Y
-var ballColor = 'rgb(0, 155, 155)'
+var ballX = canvas.width/2;
+var ballY = canvas.height/2;
+var ballColor = 'rgb(0, 155, 155)';
 var ballRadius = 50;
 
-//PHYSICS
-var speedX;
-var speedY; //going to tell me how many pixels it will move on the Y axis on the frame
-var ballRadius;
+var speedX = 5;
+var speedY = 3;
 
-function animate() {
+var directionDown = true;
+var directionRight = true;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function animate(){
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
     ctx.fillStyle = ballColor;
     ctx.strokeStyle = 'black';
-    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+    ctx.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
-    ballX = ballX + speedX;
-    ballY = ballY + speedY;
-
-    if (ballX + ballRadius >= canvas.width) {
-        directionRight = true;
-
-    }
-    if (ballX <= 0) {
-        directionRight = true;
+    if(ballX + ballRadius >= canvas.width || ballX - ballRadius <= 0 ) {
+        directionRight = !directionRight;
     }
 
-    if (directionRight) {
+    if(directionRight){
         ballX = ballX + speedX;
-    } else {
+    }
+    else {
         ballX = ballX - speedX;
     }
 
 
-
-    if (ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) { // || = OR
-        directionDown = false;
+    if(ballY + ballRadius >= canvas.height || ballY - ballRadius <= 0) {
+        directionDown = !directionDown;
     }
-    if (ballY - ballRadius <= 0) {
-        directionDown = true;
-
+    if(directionDown){
+        ballY = ballY + speedY;
+    }
+    else {
+        ballY = ballY - speedY;
     }
 
-
-    window.requestAnimationFrame(animate) //our browser is going to request the next animation frame. This will be defined by (animate) function
+    window.requestAnimationFrame(animate);
 }
 
 animate();
 
-canvas.addEventListener("click", function (e) { //the peramter that is being passed to it is that event itself. 
-            console.log(event);
+canvas.addEventListener("click", function(event){
 
-            var distX = Math.abs(ballX - event.clientX);
-            var distY = Math.abs(ballY - event.clientY);
+    var distX = Math.abs(ballX - event.clientX);
+    var distY = Math.abs(ballY - event.clientY);
 
-            if (distX < ballRadius && distY < ballRadius) {
-                console.log('FIRE@@@@@@@!!!!!!!!!!!!!')
-            }
-        }
+    if(distX < ballRadius && distY < ballRadius){
+        console.log('FIRE!!!!!!');
+    }
+})
