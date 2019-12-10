21fdff960811663fdd8c0ed804787e8672d42352
Gabe Silverstein <gabesilverstein@gmail.com>
2019-12-10 00:18:12 -0800

    Final Stage 2: C10 - removed garbage code

--- Assignment-11(Final_Stage_2)/main.js
+++ Assignment-11(Final_Stage_2)/main.js
@@ -60,37 +60,3 @@ $(window).on('load', function () {
 (function() {
     $('form > input').keyup(function() {
 
-        var empty = false;
-        $('form > input').each(function() {
-            if ($(this).val() == '') {
-                empty = true;
-            }
-        });
-
-        if (empty) {
-            $('#register').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-        } else {
-            $('#register').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-        }
-    });
-})()
-
-
-// ----------------- FORM 2
-$('#contact-form').on('blur keyup change', 'input', function(event) {
-    validateForm('#contact-form');
-  });
-  
-  function validateForm(id) {
-    var valid = $(id).validate().checkForm();
-      if (valid) {
-        $('.form-save').prop('disabled', false);
-          $('.form-save').removeClass('isDisabled');
-      } else {
-        $('.form-save').prop('disabled', 'disabled');
-        $('.form-save').addClass('isDisabled');
-      }
-  }
-  
-  // Run once, so subsequent input will be show error message upon validation
-  validateForm('#contact-form');
